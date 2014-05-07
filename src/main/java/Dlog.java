b0c250fa99c38763d682f72b61a75c1c9e9805e0
Qubo Song <sqb_007@hotmail.com>
2014-05-07 13:11:01 +0800

    Optimized BigInteger power algorithm, using the binary exponential table rather than the modPow() function. Greatly increased running speed.

--- src/main/java/Dlog.java
+++ src/main/java/Dlog.java
@@ -20,38 +20,84 @@ public class Dlog {
         BigInteger h = new BigInteger("323947510405045044356526437872806578864909752095244" +
                                       "952783479245297198197614329255807385693795855318053" +
                                       "2878928001494706097394108577585732452307673444020333");
-
-        Integer B = (int)Math.pow(2, 20);
+        Integer length = 20;
+        Integer B = (int)Math.pow(2, length);
+        BigInteger[] exp_g_invs = new BigInteger[length + 1];
+        BigInteger[] exp_gB = new BigInteger[length + 1];
         Map<BigInteger, Integer> left = new HashMap<BigInteger, Integer>(B + 1);
         Integer x0, x1;
-        BigInteger result;
+        BigInteger gTemp, leftTemp, rightTemp, result;
 
         Integer beacon = -1;
 
-        System.out.printf("Starting to calcuate Dlog_g(h) under modulo p...\n");
-        System.out.printf("Building up hash table for middle attack...\n");
+        System.out.printf("Starting to calculate Dlog_g(h) under modulo p...\n");
+        System.out.printf("Building up hash table for middle attack...");
+
+        exp_g_invs[0] = g.modInverse(p);
+        for (int iLength = 1; iLength < length + 1; iLength++) {
+            exp_g_invs[iLength] = exp_g_invs[iLength - 1].modPow(BigInteger.valueOf(2), p);
+        }
+
+
+
         for (x1 = 0; x1 < B + 1; x1++) {
-            if ((int)(x1 * 100 / B) > beacon) {
-                beacon = (int)(x1 * 100 / B);
+
+            String polynomial;
+
+            if (x1 * 100 / B > beacon) {
+                beacon = x1 * 100 / B;
                 System.out.printf("%d%% completed...\n", beacon);
                 System.out.flush();
             }
-            // power: 2^20, 20 bit.
-            left.put(h.multiply(g.modPow(new BigInteger("-" + x1.toString()), p)).mod(p), x1);
+
+            polynomial = Integer.toBinaryString(x1);
+            gTemp = BigInteger.ONE;
+
+            for (int iLength = 0; iLength < polynomial.length(); iLength++) {
+                if ('1' == polynomial.charAt(polynomial.length() - 1 - iLength)) {
+                    gTemp = gTemp.multiply(exp_g_invs[iLength]).mod(p);
+                }
+            }
+
+            leftTemp = h.multiply(gTemp).mod(p);
+
+            left.put(leftTemp, x1);
         }
 
         System.out.printf("Searching for collision...\n");
+        beacon = -1;
+        exp_gB[0] = g.modPow(BigInteger.valueOf(B), p);
+        for (int iLength = 1; iLength < length + 1; iLength++) {
+            exp_gB[iLength] = exp_gB[iLength - 1].modPow(BigInteger.valueOf(2), p);
+        }
+
         for (x0 = 0; x0 < B + 1; x0++) {
-            BigInteger right = g.modPow(new BigInteger(B.toString()), p).modPow(new BigInteger(x0.toString()), p);
-            if(left.containsKey(right)) {
+
+            String polynomial;
+
+            if (x0 * 100 / B > beacon) {
+                beacon = x0 * 100 / B;
+                System.out.printf("%d%% range covered...\n", beacon);
+                System.out.flush();
+            }
+
+            polynomial = Integer.toBinaryString(x0);
+            rightTemp = BigInteger.ONE;
+
+            for (int iLength = 0; iLength < polynomial.length(); iLength++) {
+                if ('1' == polynomial.charAt(polynomial.length() - 1 - iLength)) {
+                    rightTemp = rightTemp.multiply(exp_gB[iLength]).mod(p);
+                }
+            }
+            if(left.containsKey(rightTemp)) {
                 System.out.printf("Found...");
-                x1 = left.get(right);
+                x1 = left.get(rightTemp);
                 break;
             }
         }
 
-        result = new BigInteger(B.toString()).multiply(new BigInteger(x0.toString())).add(new BigInteger(x1.toString()));
-        System.out.printf("Dlog_g(h) is: " + result);
+        result = new BigInteger(B.toString()).multiply(BigInteger.valueOf(x0)).add(BigInteger.valueOf(x1));
+        System.out.printf("Dlog_g(h) is: " + result + "\n");
 
     }
 }
